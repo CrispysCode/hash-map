91d5f82ee729b60f48fc17a4997faa6b94e258cb
CrispysCode <pr0ject0dinx@gmail.com>
2025-03-07 14:47:44 -0600

    hash method implemented, with  a mod to apply modulo

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ class HashMap {
         this.buckets = new Array(this.capacity);
     }
 
-    
+    hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+        return hashCode;
+    }
 }
