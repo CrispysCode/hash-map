6a6f0ab8d473d5c62e16a95ceec3dd78ff7268a6
CrispysCode <pr0ject0dinx@gmail.com>
2025-03-08 03:42:41 -0600

    HashMap class now has a get method implemented

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class HashMap {
 
     hash(key) {
         let hashCode = 0;
-        let primeNumber = 31;
+        const primeNumber = 31;
 
         for (let i = 0; i < key.length; i++) {
             hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
@@ -55,5 +55,16 @@ class HashMap {
         this.capacity = newCapacity;
     }
     
+    get(key) {
+        const index = this.hash(key);
+        const bucket = this.buckets[index];
+
+        for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i][0] === key) {
+                return bucket[i][1];
+            }
+        }
+        return null;
+    }
 
 }
